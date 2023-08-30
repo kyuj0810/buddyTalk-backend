aed0ba19f26a08471f5b66622c4b41e7e8a06d55
jin <kyuj0810@gmail.com>
2023-08-30 19:07:52 +0900

    babel설정'

---
+++ server.js
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'bebe',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server
+  .listen()
+  .then(() => console.log('Server is running on http://localhost:4000/'));
