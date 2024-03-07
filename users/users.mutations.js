e40c060b73020e7d53e10f1c4dd5262e76810e6c
jin <kyuj0810@gmail.com>
2024-03-07 17:19:56 +0900

    seeProfile 생성

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -4,16 +4,23 @@ import client from '../client.js';
 export default {
   Mutation: {
     createAccount: async (_, { username, email, password }) => {
-      const existingUser = await client.user.findFirst({
-        where: {
-          OR: [{ username }, { email }],
-        },
-      });
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [{ username }, { email }],
+          },
+        });
+        if (existingUser) {
+          throw new Error('This username/email is already taken.');
+        }
 
-      const uglyPassword = await bcrypt.hash(password, 10);
-      return client.user.create({
-        data: { username, email, password: uglyPassword },
-      });
+        const uglyPassword = await bcrypt.hash(password, 10);
+        return client.user.create({
+          data: { username, email, password: uglyPassword },
+        });
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
