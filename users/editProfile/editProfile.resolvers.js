85bd66868c7e420b0a802774bcc8aab6e84d138a
jin <kyuj0810@gmail.com>
2024-03-14 17:50:15 +0900

    protectedResolver part2 add

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,35 +2,39 @@ import client from '../../client.js';
 import bcrypt from 'bcrypt';
 import { protectedResolver } from '../users.utils.js';
 
+const resolverFn = async (
+  _,
+  { email, password: newPassword },
+  { loggedInUser }
+) => {
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      email,
+      // password: uglyPassword,
+      ...(uglyPassword && { password: uglyPassword }),
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: 'Could not update profile.',
+    };
+  }
+};
+
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (_, { email, password: newPassword }, { loggedInUser }) => {
-        let uglyPassword = null;
-        if (newPassword) {
-          uglyPassword = await bcrypt.hash(newPassword, 10);
-        }
-        const updatedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            email,
-            // password: uglyPassword,
-            ...(uglyPassword && { password: uglyPassword }),
-          },
-        });
-        if (updatedUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: 'Could not update profile.',
-          };
-        }
-      }
-    ),
+    editProfile: protectedResolver(resolverFn),
   },
 };
