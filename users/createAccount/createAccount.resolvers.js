ccc6733305e961fe439bb05ebf92e005f72c61ed
jin <kyuj0810@gmail.com>
2024-03-28 00:09:26 +0900

    createAccout return값 수정

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -11,13 +11,18 @@ export default {
           },
         });
         if (existingUser) {
-          throw new Error('This username/email is already taken.');
+          throw new Error('이미 등록된 username 또는 email 입니다.');
         }
 
         const uglyPassword = await bcrypt.hash(password, 10);
-        return client.user.create({
+        const createUser = client.user.create({
           data: { username, email, password: uglyPassword },
         });
+        if (createUser) {
+          return { ok: true };
+        } else {
+          return { ok: false, error: '시스템담당자에게 문의바랍니다.' };
+        }
       } catch (e) {
         return e;
       }
